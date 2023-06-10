a9d7e618296059b163e584d09b1c6b22f731afcb
Deepak Jangir <tdeepak509@gmail.com>
2023-06-10 17:39:50 +0100

    Dynamically settting port valeus

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.use(express.static("./public"));
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
