400bb687cc77a442167980b7687c2a0a51e623d1
Deepak Jangir <tdeepak509@gmail.com>
2023-06-10 17:16:20 +0100

    Implemented async wrapper error handling

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/notFound");
+const errorHandlerMiddleware = require("./middleware/errorHandler");
+
 require("dotenv").config();
+
+// routes
+app.use("/api/v1/tasks", tasks);
+
+// middleware
+app.use(express.json());
+app.use(express.static("./public"));
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const port = 3000;
 const start = async () => {
@@ -15,10 +27,3 @@ const start = async () => {
 };
 
 start();
-
-// middleware
-app.use(express.json());
-app.use(express.static("./public"));
-
-// routes
-app.use("/api/v1/tasks", tasks);
