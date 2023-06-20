309cf3e635d39300a40c028a2eb2c5865906461b
Deepak Jangir <tdeepak509@gmail.com>
2023-06-20 10:33:56 +0100

    handling custom error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,24 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+  let customError = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went really wrong! Try again later",
+  };
 
-module.exports = errorHandlerMiddleware
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for field ${Object.keys(
+      err.keyValue
+    )}, please choose wisely!`;
+    customError.statusCode = 400;
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
