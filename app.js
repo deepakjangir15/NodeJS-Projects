7730f97a1a8c46e9746007f252a905df820e07c3
Deepak Jangir <tdeepak509@gmail.com>
2023-06-25 09:32:32 +0100

    Mongoose connection and JWT token

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+const morgan = require("morgan");
+
+// database
+const connectDB = require("./db/connect");
+
+// Routers
+const authRouter = require("./routes/authRoutes");
+
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(morgan("tiny"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Ecommerce API");
+});
+
+app.use("/api/v1/auth", authRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Listening on port ${port}`));
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
+
+start();
---
+++ controllers/authController.js
@@ -0,0 +1,34 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+const { createJWT, isTokenValid } = require("../utils");
+
+const register = async (req, res) => {
+  const { email, name, password } = req.body;
+  const emailExists = await User.findOne({ email });
+
+  if (emailExists) {
+    throw new CustomError.BadRequestError("Email already exists");
+  }
+
+  // First registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({ email, name, password, role });
+
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const token = createJWT({ payload: tokenUser });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
+};
+
+const login = async (req, res) => {
+  res.send("login user");
+};
+
+const logout = async (req, res) => {
+  res.send("logout user");
+};
+
+module.exports = { register, login, logout };
